af4901916022434f710b5a2886a6f68b6572c857
TylrPopcorn <tyler_mac@Rainbow-Trains-MacBook-Pro.local>
2023-01-17 13:19:41 -0500

    Initialized

---
+++ index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom";
+import "./styles/styles.css";
+import App from "./components/App";
+
+/**
+ * Dependencies:
+ *
+ * npm install react
+ * npm install parcel-bundler
+ * npm install react-router-dom
+ *
+ */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
